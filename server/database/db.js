7ced9b51acf12ce6b2f9671c149b7faa9b60d0da
ishvar99 <ishanvarshney99@gmail.com>
2020-05-07 12:26:58 +0530

    improved file structure

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,5 @@
 const mongoose =require('mongoose');
 const {data:{mongoURI}}=require('../config/keys')
-
 mongoose.connect(mongoURI,{useNewUrlParser:true, useUnifiedTopology: true })
 .then(()=>{
     console.log('Connected to Database!')
